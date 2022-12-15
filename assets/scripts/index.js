74cd490a8840e13140d30a5ed6b0aebdfc93fff6
MattBanuag <matthewbanuag.dev@gmail.com>
2022-12-15 12:46:04 -0600

    update login validation

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,31 +4,31 @@ import { onEvent, select, create } from "./utils.js";
 // HTML DOCUMENT BRIDGE
 const loginBtn = select('.login-btn');
 const createBtn = select('.create-btn');
-const usernameInput = select('.username-input');
-const passwordInput = select('.password-input');
 const dialogProfileSuccess = select('.dialog-success');
 const dialogProfileFailed = select('.dialog-failed');
 const dialogLoginFailed = select('.dialog-login-failed');
 const emailRegex = /^(?=^.{8,}$)[-_A-Za-z0-9]+([_.-][a-zA-Z0-9]+)*@[A-Za-z0-9]+([.-][a-zA-Z0-9]+)*\.[A-Za-z]{2,}$/;
+let usernameInput = select('.username-input');
+let passwordInput = select('.password-input');
+usernameInput.value = '';
+passwordInput.value = '';
 
 onEvent('click', createBtn, () => {
     let username = usernameInput.value;
     let password = passwordInput.value;
 
-    if(!emailRegex.test(username)) usernameInput.style.border = 'thin solid #FF4A4A';
-    if(password.length < 6) {
-        passwordInput.style.border = 'thin solid #FF4A4A'
-    } else {
-        localStorage.setItem('username', username);
-        localStorage.setItem('password', password);
+    if(!emailRegex.test(username)) return usernameInput.style.border = 'thin solid #FF4A4A';
+    if(password.length < 6) return passwordInput.style.border = 'thin solid #FF4A4A';
+        
+    localStorage.setItem('username', username);
+    localStorage.setItem('password', password);
 
-        dialogProfileSuccess.showModal();
-        setTimeout(() => {
-            dialogProfileSuccess.close();
-        }, 2000);
+    dialogProfileSuccess.showModal();
+    setTimeout(() => {
+        dialogProfileSuccess.close();
+    }, 2000);
 
-        console.log(localStorage);
-    }
+    console.log(localStorage);
 }); 
 
 // EVENTS
